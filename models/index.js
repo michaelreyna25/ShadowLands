52fa9544726a3f06d248ab73f65e96e6c14223e0
Michael Reyna <25.27.reyna@gmail.com>
2023-02-13 14:12:45 -0700

    finished index in models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const Boss = require('./Boss')
 const Captured = require('./Captured')
-const Index = require('./Index')
 const Journey = require('./Journey')
 const Location = require('./Location')
 const Player = require('./Player')
@@ -8,12 +7,13 @@ const Prototype = require('./Prototype')
 const User = require('./User')
 const Wild = require('./Wild')
 
-Boss.belongTo()
+Boss.belongTo(Location, {
+    foreignKey: 'location_id',
+});
 
-
-
-
-
+Boss.belongTo(Prototype, {
+    foreignKey: 'prototype',
+});
 
 Captured.belongTo(Player, {
     foreignKey: 'player_id',
@@ -26,5 +26,64 @@ Captured.belongTo(Prototype, {
 });
 
 Journey.hasMany(Player, {
+    foreignKey: 'player_id',
+});
 
-})
+Journey.belongTo(Location, {
+    foreignKey: 'location_id',
+});
+
+Location.hasMany(Player, {
+    foreignKey: 'player_id',
+    onDelete: 'CASCADE'
+});
+
+Player.belongTo(Location, {
+    foreignKey: 'location_id',
+    onDelete: 'CASCADE'
+});
+
+Player.hasOne(Captured, {
+    foreignKey: 'captured_id',
+    onDelete: 'CASCADE'
+});
+
+Prototype.hasMany(Captured, {
+    foreignKey: 'captured_id',
+    onDelete: 'CASCADE'
+});
+
+Prototype.hasMany(Wild, {
+    foreignKey: 'wild_id',
+    onDelete: 'CASCADE'
+});
+
+Prototype.hasMany(Boss, {
+    foreignKey: 'boss_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasOne(Player, {
+    foreignKey: 'player_id',
+    onDelete: 'CASCADE'
+});
+
+Wild.belongTo(Location, {
+    foreignKey: 'location_id',
+    onDelete: 'CASCADE'
+});
+
+Wild.belongTo(Prototype, {
+    foreignKey: 'prototype_id',
+});
+
+module.exports = {
+    Player,
+    Boss,
+    Captured,
+    Journey,
+    Location,
+    User,
+    Wild,
+    Prototype
+}
