c04e94f038a92adea621e020b3bb5f7b62b86e86
troynj <troynjohnson6@gmail.com>
2023-02-14 01:45:02 -0800

    adding location api route

---
+++ controllers/api/location.js
@@ -0,0 +1,95 @@
+const router = require("express").Router();
+const withAuth = require('../../utils/auth');
+
+const { Location } = require("../../models");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const LocationArr = await Location.findAll();
+    // const captured = capturedArr.map((captured) => captured.get({ plain: true }));
+
+    res.status(200).json(LocationArr);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get("/:id", withAuth, async (req, res) => {
+  const { id } = req.params;
+  console.log(req.params.id);
+  try {
+    const locationArr = await Location.findByPk(id);
+    console.log(locationArr);
+    if (!locationArr) {
+      res
+        .status(404)
+        .json({ message: "No Location found with that id!" });
+      return;
+    }
+
+    res.status(200).json(locationArr);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newLocation = await Location.create(req.body);
+    res.status(200).json(newLocation);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newLocation = await Location.create(req.body);
+    res.status(200).json(newLocation);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [updated] = await Location.update(req.body, { where: { id }});
+
+    if (!updated) {
+      return res
+        .status(404)
+        .json({ message: "No Location with that ID can be updated with the given information"});
+    }
+
+    const updatedLocation = await Location.findByPk(id);
+    return res.status(200).json(updatedLocation);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  // delete a Location by its `id` value
+  try {
+    // Delete the Location with the given `id` from the database
+    const deleted = await Location.destroy({ where: { id: req.params.id } });
+
+    // If the Location is not found, return a 404 response with a message
+    if (!deleted) {
+      return res.status(404).json({ message: "Location not found" });
+    }
+
+    // const updatedLocation = await Location.findAll();
+    // return res.status(200).json(updatedLocation);
+    
+    // Return a success message in the response
+    return res.status(200).json({ message: "Location deleted successfully" });
+  } catch (err) {
+    // In case of any errors, return a 400 response with the error message
+    return res.status(400).json(err);
+  }
+});
+
+module.exports = router;
