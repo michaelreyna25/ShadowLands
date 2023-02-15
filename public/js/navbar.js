0b68e643f0aaa9d0ec5f72ea76209855b28447d9
Michael Reyna <25.27.reyna@gmail.com>
2023-02-14 23:55:43 -0700

    added functionality to logout button

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,25 @@
+
+var backDrop = document.getElementById('#backDrop');
+var myLogOut = document.getElementById('#logout');
+var closeBttn = document.getElementsByClassName('.close');
+
+myLogOut.onClick = () => {
+  console.log('yes');
+  backDrop.style.display = 'block';
+}
+
+closeBttn.onclick = () => {
+  backDrop.style.display = 'none';
+}
+
+window.onClick = (event) => {
+  if (event.target == backDrop) {
+    backDrop.style.display = 'none';
+  }
+}
+
 const logout = async () => {
-    const response = await fetch('/api/user', {
+  const response = await fetch('/api/user', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
@@ -11,4 +31,4 @@ const logout = async () => {
   }
 }
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector('#logoutConfirm').addEventListener('click', logout);
