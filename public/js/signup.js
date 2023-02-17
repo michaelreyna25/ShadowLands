6df4f9d1db9964ca194e6b5321ec77c91d0b52ae
Michael Reyna <25.27.reyna@gmail.com>
2023-02-17 00:52:36 -0700

    create login and signup hbs along with js and css

---
+++ public/js/signup.js
@@ -0,0 +1,9 @@
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const confirmPassword = document.querySelector('.confirm-password');
+const confirm = document.querySelector('.confirm');
+
+// confirm password
+// Create user 
+// save user to database
+// 
