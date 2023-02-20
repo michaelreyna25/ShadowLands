025fe824ddbb746e4dbfe6659b115a8ba3154c2c
Michael Reyna <25.27.reyna@gmail.com>
2023-02-20 15:55:05 -0700

    add routes to homepage

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
+const { User, Player } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
     res.render("home", {});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard/:id', async (req, res) => {
+  try {
+    const dashboardData = await Player.findByPk(req.params.id.
+      player_id, {
+      attributes: { exclude: ["user_id"] },
+      include: [{ model: Location }]
+    });
+    const dashboard = dashboardData.get({ plain: true });
+    res.render('dashboard', {
+      ...dashboard,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
